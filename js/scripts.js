5d76b609fb1d022bb8a9edb36b6dfcdc7b48c290
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2021-12-07 14:13:01 +0100

    adding array of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood ='Wareniki';
-document.write(favoriteFood);
+let pokemonList [
+  {name: 'Bulbasaur' , type: 'grass' , height: 2.04 };
+  {name: 'Charmeleon' , type: 'fire' , height: 2.00 };
+  {name: 'Squirtle' , type: 'water' , height: 1.08 };
+];
