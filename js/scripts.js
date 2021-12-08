3a70b77c0b848922a4fd58f69bbdaa950961cbc2
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2021-12-08 15:18:15 +0100

    creating loop for Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,21 @@
-let pokemonList [
-  {name: 'Bulbasaur' , type: 'grass' , height: 2.04 };
-  {name: 'Charmeleon' , type: 'fire' , height: 2.00 };
-  {name: 'Squirtle' , type: 'water' , height: 1.08 };
+let pokemonList= [
+  {name: 'Bulbasaur', types: ['grass'], height: 2.04},
+  {name: 'Charmeleon', types: ['fire'], height: 2.00},
+  {name: 'Squirtle', types: ['water'], height: 1.08},
 ];
+
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList[i].types ='grass'){
+    console.log(pokemonList[i].name);
+  }
+  else if (pokemonList[i].types ='fire'){
+    console.log(pokemonList[i].name);
+  }
+  else {
+    console.log(pokemonList[i].name);
+  }
+if (pokemonList[i].height > 2) {
+  console.log( ' Wow, that\'s big!')
+}
+  document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ')')
+}
