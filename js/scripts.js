381a125a0c4ee56efd3990aba198e1184ae17c62
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2021-12-10 14:40:29 +0100

    wrapping everything into IIEF

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
-let pokemonList= [
-  {name: 'Bulbasaur', types: ['grass'], height: 2.04},
-  {name: 'Charmeleon', types: ['fire'], height: 2.00},
-  {name: 'Squirtle', types: ['water'], height: 1.08},
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Bulbasaur', types: ['grass'], height: 2.04},
+    {name: 'Charmeleon', types: ['fire'], height: 2.00},
+    {name: 'Squirtle', types: ['water'], height: 1.08},
+  ];
 
-pokemonList.forEach((item, i) => {
-  document.write('<br>' + pokemonList[i].name + '(height:' + pokemonList[i].height + ') <br>');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+pokemonRepository.add({name: 'Vulpix', types:['fire'], height: 2.00 });
+
+pokemonRepository.getAll().forEach(function(item, i) {
 });
