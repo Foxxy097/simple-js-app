4280ec41c17a7a95b9f9c629b9772ab41dc1e46a
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2021-12-13 17:33:02 +0100

    add new function and display of pokemon as button

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,30 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
+function addListItem(pokemon){
+  let pokemonList = document.querySelector('.pokemon-list');
+  let listpokemon = document.createElement('li');
+  let button = document.createElement('button');
+  button.innerText = pokemon.name;
+  button.classList.add('Pokemon-button');
+  listpokemon.appendChild(button);
+  pokemonList.appendChild(listpokemon);
 
+  button.addEventListener('click', function(event) {
+  showDetails(pokemon);
+  });
+}
+function showDetails(pokemon){
+  console.log(pokemon.name)
+}
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 pokemonRepository.add({name: 'Vulpix', types:['fire'], height: 2.00 });
 
-pokemonRepository.getAll().forEach(function(item, i) {
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
