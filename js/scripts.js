4404c0b4f55015c0080af2b2c8ad5c8db15f4538
Foxxy097 <95033090+Foxxy097@users.noreply.github.com>
2021-12-15 05:01:47 +0100

    added modal to preview pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let modalContainer = document.querySelector('#modal-container');
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -54,10 +55,57 @@ function loadDetails(item) {
 }
 
 function showDetails(pokemon){
-  loadDetails(pokemon).then(function(){
-  console.log(pokemon)
+  pokemonRepository.loadDetails(pokemon).then(function () {
+  showModal(pokemon);
   });
 }
+
+function showModal(pokemon) {
+  modalContainer.innerHTML = '';
+
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  let closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+
+  let titleElement = document.createElement('h2');
+  titleElement.innerText = pokemon.name;
+
+  let contentElement = document.createElement('p');
+  contentElement.innerText = 'Height: ' + pokemon.height;
+
+  let imageElement = document.createElement('img');
+  imageElement.src = pokemon.imageUrl;
+
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(contentElement);
+  modal.appendChild(imageElement);
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+}
+
+function hideModal() {
+  modalContainer.classList.remove('is-visible');
+}
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+    hideModal();
+  }
+});
+
+modalContainer.addEventListener('click', (e) => {
+  let target = e.target;
+  if (target === modalContainer) {
+    hideModal();
+  }
+});
 
   return {
     add: add,
@@ -69,8 +117,7 @@ function showDetails(pokemon){
   };
 })();
 pokemonRepository.loadList().then(function(){
-
-  pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
